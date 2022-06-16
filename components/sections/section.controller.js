cfb1886606b93339b149555ff20367eb61a552fb
Mayowa <mayority11@gmail.com>
2022-06-16 22:58:33 +0100

    .

--- components/sections/section.controller.js
+++ components/sections/section.controller.js
@@ -1,19 +1,19 @@
-import Section from "./section.model.js";
-import { BadRequestError } from "../../utils/errors.js";
+import Section from './section.model.js'
+import { BadRequestError } from '../../utils/errors.js'
 
 export const createSection = async (req, res) => {
-  const { title } = req.body;
-  const activeSession = await Section.findOne({ status: "active" });
-  if (activeSession) {
-    throw new BadRequestError(
-      "Cannot create new session when there is still an active session"
-    );
-  }
-  const section = await Section.create({ title });
-  res.status(201).json({ section });
-};
+  const { title } = req.body
+  // const activeSession = await Section.findOne({ status: 'active' })
+  // if (activeSession) {
+  //   throw new BadRequestError(
+  //     'Cannot create new section when there is still an active session',
+  //   )
+  // }
+  const section = await Section.create({ title })
+  res.status(201).json({ section })
+}
 
 export const getAllSections = async (req, res) => {
-  const sections = await Section.find();
-  res.status(200).json({ sections });
-};
+  const sections = await Section.find()
+  res.status(200).json({ sections })
+}
